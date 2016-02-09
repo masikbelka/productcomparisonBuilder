7a7a6d3795d2f2c72f1247ed8af803df7e50d73a
Mykyta Pavlenko <nikitapavlenko@ukr.net>
2016-02-10 01:02:14 +0200

    initial commit

---
+++ app/builder-app.js
@@ -0,0 +1,4 @@
+'use strict';
+angular.module('epam.prodcomparison.builder', [
+    'builder'
+]);
---
+++ app/comparison-configuration/controllers/comparison-configuration-controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('epam.prodcomparison.builder')
+    .controller('ComparisonConfigurationController', ['$scope', 'Restangular', '$location',
+        function ($scope, Restangular, $location) {
+        }
+    ]);
+
+
