e6445a01132eddd09726b4f748ee9d4389886ad5
Mykyta Pavlenko <mykyta_pavlenko@epam.com>
2016-02-22 12:02:04 +0200

    config added

--- app/comparison-configuration/controllers/comparison-configuration-controller.js
+++ app/comparison-configuration/controllers/comparison-configuration-controller.js
@@ -1,9 +1,36 @@
 'use strict';
 
 angular.module('epam.prodcomparison.builder')
-    .controller('ComparisonConfigurationController', ['$scope', 'Restangular', '$location',
-        function ($scope, Restangular, $location) {
-        }
+    .controller('ComparisonConfigurationController', ['$scope', 'ProductComparisonRest', 
+        function ($scope, ProductComparisonRest) {
+    		
+    	$scope.maxInComparison = 1; 
+    	$scope.defaultSortingStategy = ""; 
+    	
+    	var maxProductsNumberKey = "max_products_number";
+    	
+    	ProductComparisonRest.comparison
+	        .one('config', maxProductsNumberKey)
+	        .get()
+	        .then(function(data) {
+	        	$scope.maxInComparison = data.config_value;
+	        });
+    	}
+    
+    
+    	function updateMaxProduct(){
+    		ProductComparisonRest.comparison
+	        .one('config', maxProductsNumberKey)
+	        .post({
+	        	"config_id": maxProductsNumberKey,
+	        	"config_value": $scope.maxProductsNumberKey
+	        })
+	        .then(function(data) {
+	        	alert("config was succesfully updated");
+	        });
+    	}
+    	}
+    
     ]);
 
 
---
+++ app/comparison-configuration/services/productcomparison-rest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('epam.prodcomparison.builder')
+    .factory('ProductComparisonRest', ['Restangular', function(Restangular) {
+        return {
+            comparison: Restangular.withConfig(function(RestangularConfigurer) {
+                RestangularConfigurer.setBaseUrl('https://api.yaas.io/epam/productcomparison/dev/');
+            })
+        };
+    }]);
