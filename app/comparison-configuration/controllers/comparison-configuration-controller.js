adfc3af40d3c948aa11e06a7e5c6a29f968aa7c6
Mykyta Pavlenko <mykyta_pavlenko@epam.com>
2016-02-23 22:05:57 +0200

    configuration added

--- app/comparison-configuration/controllers/comparison-configuration-controller.js
+++ app/comparison-configuration/controllers/comparison-configuration-controller.js
@@ -1,34 +1,65 @@
 'use strict';
 
 angular.module('epam.prodcomparison.builder').controller(
-		'ComparisonConfigurationController',
-		[
-				'$scope',
-				'ProductComparisonRest',
-				function($scope, ProductComparisonRest) {
+    'ComparisonConfigurationController',
+    [
+        '$scope',
+        'ProductComparisonRest',
+        function ($scope, ProductComparisonRest) {
 
-					$scope.comparisonConfig = {
-					    "config_id" : "max_products_number",
-					    "config_value": "1"
-					}
+            $scope.maxInComparisonConfig = {
+                "config_id": "max_products_number",
+                "config_value": "1"
+            };
+
+            $scope.defaultSortingStrategy = {
+                "config_id": "default_sorting_strategy",
+                "config_value": "a-z"
+            };
+
+            $scope.sortingOptions = [
+                {title: "Common to the top", code: "commonTop"},
+                {title: "Common to the dow", code: "commonDown"},
+                {title: "Alphabetical(A-Z)", code: "a-z"},
+                {title: "Alphabetical(Z-A)", code: "z-a"}];
 
 
-					ProductComparisonRest.comparison
-					.one('configs', "max_products_number")
-					.get()
-					.then(function(configFromServer) {
-						$scope.comparisonConfig.config_value = configFromServer.config_value;
-					}, function(response) {
-						console.log("Error with status code", response.status);
-						$scope.comparisonConfig.config_value = 1;
-					});
+            ProductComparisonRest.comparison
+                .one('configs', "max_products_number")
+                .get()
+                .then(function (configFromServer) {
+                    $scope.maxInComparisonConfig.config_value = configFromServer.config_value;
+                }, function (response) {
+                    console.log("Error with status code", response.status);
+                    $scope.maxInComparisonConfig.config_value = 1;
+                });
 
-					$scope.updateMaxProduct = function() {
-						ProductComparisonRest.comparison
-						.all("configs/max_products_number")
-						.post($scope.comparisonConfig)
-						.then(function(data) {
-							alert("config was succesfully updated");
-						});
-					}
-				} ]);
+            ProductComparisonRest.comparison
+                .one('configs', "default_sorting_strategy")
+                .get()
+                .then(function (configFromServer) {
+                    $scope.defaultSortingStrategy.config_value = configFromServer.config_value;
+                }, function (response) {
+                    console.log("Error with status code", response.status);
+                    $scope.defaultSortingStrategy.config_value = "a-z";
+                });
+
+            $scope.updateMaxProduct = function () {
+                ProductComparisonRest.comparison
+                    .all("configs/max_products_number")
+                    .post($scope.maxInComparisonConfig)
+                    .then(function (data) {
+                        alert("Max in comparison config was successfully updated");
+                    });
+                }
+
+            $scope.updateDefaultSortingStrategy = function(){
+                ProductComparisonRest.comparison
+                    .all("configs/default_sorting_strategy")
+                    .post($scope.defaultSortingStrategy)
+                    .then(function (data) {
+                        alert("Default sorting strategy config was successfully updated");
+                    });
+            }
+
+        }]);
