f97ac1a5968d2d743d7a2d2ee5c4e396746e077f
Mykyta Pavlenko <mykyta_pavlenko@epam.com>
2016-03-13 22:53:36 +0200

    add new configs to ui

--- app/comparison-configuration/controllers/comparison-configuration-controller.js
+++ app/comparison-configuration/controllers/comparison-configuration-controller.js
@@ -17,6 +17,12 @@ angular.module('epam.prodcomparison.builder').controller(
                 {title: "Alphabetical(A-Z)", code: "a-z"},
                 {title: "Alphabetical(Z-A)", code: "z-a"}];
 
+            $scope.expirationTimeOptions = [
+                {title: "15 min", code: "15"},
+                {title: "30 min", code: "30"},
+                {title: "1 hour", code: "60"},
+                {title: "1 day",  code: "1440"}
+            ];
 
             configSvc.getCurrentConfigValue("max_products_number").then(function (maxComparisonConfig) {
                 $scope.maxInComparison = maxComparisonConfig.config_value;
@@ -32,12 +38,40 @@ angular.module('epam.prodcomparison.builder').controller(
                 $scope.defaultSorting = "a-z";
                 $scope.old_defaultSorting = "a-z";
             });
+            configSvc.getCurrentConfigValue("hide_empty_attributes").then(function (config) {
+                $scope.hideEmpty = config.config_value;
+                $scope.old_hideEmpty = config.config_value;
+            }, function () {
+                $scope.hideEmpty = true;
+                $scope.old_hideEmpty = true;
+            });
+            configSvc.getCurrentConfigValue("hide_equal_attributes").then(function (config) {
+                $scope.hideEqual = config.config_value;
+                $scope.old_hideEqual = config.config_value;
+            }, function () {
+                $scope.hideEqual = false;
+                $scope.old_hideEqual = false;
+            });
+            configSvc.getCurrentConfigValue("ttl_anonymous_comparisons").then(function (config) {
+                $scope.ttlAnonymous = config.config_value;
+                $scope.old_ttlAnonymous = config.config_value;
+            }, function () {
+                $scope.ttlAnonymous = "15";
+                $scope.old_ttlAnonymous = "15";
+            });
+            configSvc.getCurrentConfigValue("ttl_logged_comparisons").then(function (config) {
+                $scope.ttlLogged = config.config_value;
+                $scope.old_ttlLogged = config.config_value;
+            }, function () {
+                $scope.ttlLogged = "15";
+                $scope.old_ttlLogged = "15";
+            });
 
             $scope.change = function (configId) {
                 var newValue = $scope[configId];
                 var oldValue = $scope['old_' + configId];
                 if (oldValue !== newValue) {
-                    $scope.saveButtonEnabled = 'true';
+                    $scope.saveButtonEnabled = true;
                 }
             }
 
@@ -48,6 +82,10 @@ angular.module('epam.prodcomparison.builder').controller(
             $scope.cancel = function () {
                 $scope.maxInComparison = $scope.old_maxInComparison;
                 $scope.defaultSorting = $scope.old_defaultSorting;
+                $scope.hideEmpty = $scope.old_hideEmpty;
+                $scope.hideEqual = $scope.old_hideEqual;
+                $scope.ttlAnonymous = $scope.old_ttlAnonymous;
+                $scope.ttlLogged = $scope.ttlLogged;
                 $scope.saveButtonEnabled = false;
             }
 
@@ -55,8 +93,12 @@ angular.module('epam.prodcomparison.builder').controller(
                 $scope.spinerEnabled = true;
                 var future1 = configSvc.postNewConfigValue('max_products_number', $scope.maxInComparison);
                 var future2 = configSvc.postNewConfigValue('default_sorting', $scope.defaultSorting);
+                var future3 = configSvc.postNewConfigValue('hide_empty_attributes', $scope.hideEmpty);
+                var future4 = configSvc.postNewConfigValue('hide_equal_attributes', $scope.hideEqual);
+                var future5 = configSvc.postNewConfigValue('ttl_anonymous_comparisons', $scope.ttlAnonymous);
+                var future6 = configSvc.postNewConfigValue('ttl_logged_comparisons', $scope.ttlLogged);
 
-                $q.all([future1, future2]).then(function () {
+                $q.all([future1, future2, future3, future4, future5, future6]).then(function () {
                     $scope.saveButtonEnabled = false;
                     $scope.spinerEnabled = false;
                 });
