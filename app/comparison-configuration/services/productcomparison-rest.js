6273d66f0d114782dc3b1da89f441d73fc3a7ec5
mykyta_pavlenko <mykyta_pavlenko@epam.com>
2016-04-27 19:47:08 +0300

    moved to production url

--- app/comparison-configuration/services/productcomparison-rest.js
+++ app/comparison-configuration/services/productcomparison-rest.js
@@ -4,7 +4,7 @@ angular.module('epam.prodcomparison.builder')
     .factory('ProductComparisonRest', ['Restangular', function(Restangular) {
         return {
             comparison: Restangular.withConfig(function(RestangularConfigurer) {
-                RestangularConfigurer.setBaseUrl('https://api.yaas.io/epam/productcomparison/dev/');
+                RestangularConfigurer.setBaseUrl('https://api.yaas.io/epam/productcomparison/b1/');
             })
         };
     }]);
